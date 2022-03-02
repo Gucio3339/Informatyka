f98e2d835ea579123cc480a269b9493e7f8aff29
astasiak <wiktor10392@gmail.com>
2022-03-02 12:03:19 +0100

    Liczby Pierwsze

---
+++ Liczby pierwsze/Liczby pierwsze.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Generowanie liczb pierwszych (n)
+    int ile;
+    cout << "Ile liczb 'n' wylosowac: ";
+    cin >> ile;
+
+    int n, i, p, lp;
+    bool t;
+
+    cin >> n;
+
+    lp = 0;
+    p = 2;
+    while (lp < n)
+    {
+        t = true;
+        for (i = 2; i < p; i++)
+            if (p % i == 0)
+            {
+                t = false;
+                break;
+            }
+        if (t)
+        {
+            cout << p << " ";
+            lp++;
+        }
+        p++;
+    }
+    cout << endl;
+    return 0;
+}
