e0bc4bb9f4d82a9480e00503a250beaf61ac8044
astasiak <wiktor10392@gmail.com>
2022-03-23 12:02:09 +0100

    NWW i NWD kod

---
+++ NWW i NWD/Informatyka 16.03.2022/Informatyka 16.03.2022.cpp
@@ -0,0 +1,37 @@
+﻿using namespace std;
+
+#include <iostream>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int NWW;
+
+    cout << "Podaj Pierwszą Liczbę: ";
+    cin >> a;
+
+    cout << "Podaj Drugą Liczbę: ";
+    cin >> b;
+
+    c = a * b;
+
+    while (a != b)
+    {
+        if (a < b)
+        {
+            b = b - a;
+        }
+        else if (b < a)
+        {
+            a = a - b;
+        }
+    }
+
+    NWW = c / a;
+
+    cout << "Największy Wspólny Dzielnik wynosi: " << a << endl;
+    cout << "Największa Wspólna Wielokrotność wynosi: " << NWW;
+    cout << "Koniec programu.";
+}
