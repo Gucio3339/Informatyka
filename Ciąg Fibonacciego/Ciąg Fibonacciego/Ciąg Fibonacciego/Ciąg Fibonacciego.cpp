16df6bd8ad9073958fafef57545e3785dbd20af0
astasiak <wiktor10392@gmail.com>
2022-04-27 11:37:48 +0200

    Ciąg Fibonacciego

---
+++ Ciąg Fibonacciego/Ciąg Fibonacciego/Ciąg Fibonacciego/Ciąg Fibonacciego.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+using namespace std;
+
+//CF - Ciąg Fibonacciego
+
+void Ciąg_Fibonacciego(int CF)
+{
+    cout << 0;
+    cout << ",";
+    int a = 0;
+    int b = 1;
+    for (int i = 0; i < CF; i++)
+    {
+        cout << endl;
+        cout << b << ", ";
+        b += a;
+        a = b - a;
+    }
+    cout << ".";
+}
+int main()
+{
+    int CF;
+    cout << "Jak długi ma zostać wyświetlony ciąg Fibonacciego?? (Podaj wartość z ilu znaków ma być Ciąg Fibonacciego): ";
+    cin >> CF;
+    Ciąg_Fibonacciego(CF);
+    return 0;
+}
