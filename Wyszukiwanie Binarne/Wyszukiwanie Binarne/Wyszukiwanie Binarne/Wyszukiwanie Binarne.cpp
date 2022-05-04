af96291f6da3adbf4fe3fa181f726af6ec6117e7
astasiak <wiktor10392@gmail.com>
2022-05-04 11:58:32 +0200

    04.05.2022

--- Wyszukiwanie Binarne/Wyszukiwanie Binarne/Wyszukiwanie Binarne/Wyszukiwanie Binarne.cpp
+++ Wyszukiwanie Binarne/Wyszukiwanie Binarne/Wyszukiwanie Binarne/Wyszukiwanie Binarne.cpp
@@ -1,9 +1,31 @@
-﻿#include<iostream>
+﻿#include <iostream>
+
 using namespace std;
 
 int main()
 {
-	int l = 0;
-	int p = 7;
-	int sr = (l + p) / 2;
+
+    int f0 = 0;
+    int f1 = 1;
+    int f;
+    int l;
+
+    cout << "Ile wyrazów Ciągu Wpisać: ";
+    cin >> l;
+
+    for (int i = 0; i <= l; i++)
+    {
+        if (i > 1)
+        {
+            f = f0 + f1;
+            f0 = f1;
+            f1 = f;
+            cout << f << endl;
+        }
+        else
+        {
+            f = i;
+            cout << f << endl;
+        }
+    }
 }
